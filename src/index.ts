03d6f81f68e69de29ec578b2405776cafe81f918
agata-dolatowska <agata.dolatowska@gmail.com>
2019-12-09 17:19:24 +0100

    add fill page

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import "./css/style.scss";
 import Page from "./page";
+
 const page = new Page();
--- src/page.ts
+++ src/page.ts
@@ -3,22 +3,31 @@ import { config } from ".././config";
 export default class Page {
   apiUrl: string = "https://api.scripture.api.bible/v1/bibles";
   bibleLang: string = "1c9761e0230da6e0-01";
+  chaptersList: string[];
+  currentChapter: number = 0;
+  versesList: string[];
+  currentVerse: number = 0;
   constructor() {
     this.render();
-    this.getChapter();
+    this.getAllChaptersNames();
   }
 
-  async getChapter() {
+  async getAllChaptersNames() {
     const getChapter = await fetch(`${this.apiUrl}/${this.bibleLang}/books`, {
       headers: {
         "api-key": config.bibleApiKey
       }
     });
     const chapterRes = await getChapter.json();
-    const chapterName = chapterRes.data[0].id;
+    this.chaptersList = chapterRes.data.map((el: any) => el.id);
+    this.getAllVersesIdsFromChapter();
+  }
 
+  async getAllVersesIdsFromChapter() {
     const getChapterId = await fetch(
-      `${this.apiUrl}/${this.bibleLang}/books/${chapterName}/chapters`,
+      `${this.apiUrl}/${this.bibleLang}/books/${
+        this.chaptersList[this.currentChapter]
+      }/chapters`,
       {
         headers: {
           "api-key": config.bibleApiKey
@@ -26,13 +35,13 @@ export default class Page {
       }
     );
     const chapterIdRes = await getChapterId.json();
-    const chapterId = `${chapterIdRes.data[0].id}`;
-    this.getPage(chapterId);
+    this.versesList = chapterIdRes.data.map((el: any) => el.id);
+    this.getVerseText(this.versesList[this.currentVerse]);
   }
 
-  async getPage(chapterId: string) {
+  async getVerseText(verseId: string) {
     const res = await fetch(
-      `${this.apiUrl}/${this.bibleLang}/chapters/${chapterId}`,
+      `${this.apiUrl}/${this.bibleLang}/chapters/${verseId}`,
       {
         headers: {
           "api-key": config.bibleApiKey
@@ -41,13 +50,26 @@ export default class Page {
     );
     const jsonRes = await res.json();
     document
-      .querySelector(".page")
-      .insertAdjacentHTML("afterbegin", JSON.stringify(jsonRes.data.content));
+      .querySelector(".page-text")
+      .insertAdjacentHTML("beforeend", JSON.stringify(jsonRes.data.content));
+    this.fillPage();
+  }
+
+  fillPage() {
+    if (
+      document.querySelector(".page-text").clientHeight <
+      document.querySelector(".page").clientHeight - 50
+    ) {
+      // this.currentVerse += 1;
+      console.log(this.currentVerse);
+      this.getVerseText(this.versesList[this.currentVerse]);
+    }
   }
 
   render() {
     const html: string = `
         <div class='page'>
+          <div class='page-text'></div>
         </div>
         <button>Previous</button>
         <button>Next</button>
