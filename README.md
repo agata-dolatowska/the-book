45630ecffdc66d98d2f5c5e109893b2c95c5a894
agata-dolatowska <agata.dolatowska@gmail.com>
2019-12-17 18:37:35 +0100

    add readme, previous pages ;-;

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # the-book
+
+## Install
+
+install [node.js](https://nodejs.org/en/download/)
+
+```
+git clone https://github.com/agata-dolatowska/the-book
+cd the-book
+npm install
+npm start
+```
--- src/page.ts
+++ src/page.ts
@@ -9,13 +9,32 @@ export default class Page {
   currentChapter: number = 0;
   versesList: string[];
   currentVerse: number = 0;
+  firstText: string;
+  readingDirection: string = "forward";
+  textsOnPage: number;
   constructor() {
     this.render();
     this.getAllBookNames();
     document
+      .querySelector(".previous")
+      .addEventListener("click", () => this.previousPage());
+    document
       .querySelector(".next")
       .addEventListener("click", () => this.nextPage());
   }
+
+  previousPage() {
+    if (
+      document.querySelector(".page-text").firstChild.textContent !=
+      this.firstText
+    ) {
+      document.querySelector(".page-text").innerHTML = "";
+      this.readingDirection = "backward";
+      this.textsOnPage = document.querySelector(".page-text").children.length;
+      this.fillPageReverse();
+    }
+  }
+
   nextPage() {
     if (
       !(
@@ -25,6 +44,7 @@ export default class Page {
       )
     ) {
       document.querySelector(".page-text").innerHTML = "";
+      this.readingDirection = "forward";
       this.fillPage();
     }
   }
@@ -69,17 +89,30 @@ export default class Page {
     const chapterRes: any = await getChapterData.json();
     this.versesList = chapterRes.data.content.split("</p>");
     if (chapterRes.data.number == "intro") {
-      let bookTitle: string = this.versesList[0].replace('p class="mt1"', "h1");
-      bookTitle += "</h1>";
+      let bookTitle: string = this.versesList[0].replace(
+        '<p class="mt1">',
+        "<h1>"
+      );
+      bookTitle.replace("</p>", "</h1>");
+      if (this.currentChapter == 0) {
+        this.firstText = bookTitle.replace("<h1>", "");
+      }
+      bookTitle = `${bookTitle}`;
       this.versesList[0] = bookTitle;
     } else {
       this.versesList.unshift(`<h2>${chapterRes.data.reference}</h2>`);
     }
-    this.currentVerse = 0;
-    this.fillPage();
+    if (this.readingDirection == "forward") {
+      this.currentVerse = 0;
+      this.fillPage();
+    } else {
+      this.currentVerse = this.versesList.length - 1;
+      this.fillPageReverse();
+    }
   }
 
   fillPage() {
+    console.log(this.currentVerse);
     while (
       document.querySelector(".page-text").clientHeight <
         document.querySelector(".page").clientHeight &&
@@ -91,10 +124,9 @@ export default class Page {
         .querySelector(".page-text")
         .insertAdjacentHTML(
           "beforeend",
-          `${this.versesList[this.currentVerse]}</p>`
+          `${this.versesList[this.currentVerse]}`
         );
       this.currentVerse += 1;
-
       if (
         this.currentVerse == this.versesList.length - 1 &&
         this.currentChapter < this.chaptersList.length - 1
@@ -123,6 +155,50 @@ export default class Page {
       if (this.currentVerse > 0) {
         this.currentVerse -= 1;
       }
+    }
+    console.log(this.currentVerse);
+    console.log(">>>>>>>>>");
+  }
+
+  fillPageReverse() {
+    while (
+      document.querySelector(".page-text").clientHeight <
+        document.querySelector(".page").clientHeight &&
+      this.currentVerse >= 0 &&
+      this.currentChapter >= 0 &&
+      this.currentBook >= 0
+    ) {
+      this.currentVerse -= 1;
+      // if (
+      //   // this.currentVerse == 0 && this.currentChapter!=0
+      //   this.currentVerse - this.textsOnPage == 0 &&
+      //   document.querySelector(".page-text").firstChild.textContent !=
+      //     this.firstText
+      // ) {
+      //   console.log("dawaj rozdział");
+      //   this.currentChapter -= 1;
+      //   this.textsOnPage = document.querySelector(".page-text").children.length;
+      //   this.getChapter();
+      // }
+
+      document
+        .querySelector(".page-text")
+        .insertAdjacentHTML(
+          "afterbegin",
+          `${this.versesList[this.currentVerse - this.textsOnPage]}`
+        );
+
+      // if (
+      //   document.querySelector(".page-text").clientHeight >
+      //   document.querySelector(".page").clientHeight
+      // ) {
+      //   console.log("większe");
+      //   document
+      //     .querySelector(".page-text")
+      //     .removeChild(document.querySelector(".page-text").firstChild);
+      //   this.currentVerse += 1;
+      // }
+      this.textsOnPage = document.querySelector(".page-text").children.length;
     }
   }
 
